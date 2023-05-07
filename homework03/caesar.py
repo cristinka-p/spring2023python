ee54afcb9c39d8a4baa2cb2c9f25e04ad190c09c
ponkr <ponkratova.70@mail.ru>
2023-05-07 18:03:37 +0300

    caesar

--- homework03/caesar.py
+++ homework03/caesar.py
@@ -1,9 +1,10 @@
 import typing as tp
 
-en_abc = 'abcdefghijklmnopqrstuvwxyzabc'
-en_ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABC'
-ru_abc = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяабв'
-ru_Abc = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВ'
+en_abc = 'abcdefghijklmnopqrstuvwxyz'
+en_ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ru_abc = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+ru_ABC = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+alphabet = en_abc + en_ABC + ru_abc + ru_ABC
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
@@ -18,19 +19,27 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    if plaintext.islower():
-        for i in plaintext:
-            a = en_abc.index('i')
-            ciphertext = + en_abc[a+3]
-    if plaintext.isupper():
-        for i in plaintext:
-            a = en_ABC.index('i')
-            ciphertext = + en_ABC[a+3]
-    print ciphertext
+    #en_a = str.maketrans(en_abc[ : -shift], en_abc[shift - 1 : ])
+    #en_A = str.maketrans(en_ABC[ : -shift], en_ABC[shift - 1 : ])
+    #ru_a = str.maketrans(ru_abc[ : -shift], ru_abc[shift - 1 : ])
+    #ru_A = str.maketrans(ru_ABC[ : -shift], ru_ABC[shift - 1 : ])
+
+    #trans = str.maketrans ({en_abc[i]:en_abc[i+shift],    en_ABC[i]:en_ABC[i+shift],   ru_abc[i]:ru_abc[i+shift],   ru_ABC[i]:ru_ABC[i+shift]}, for i in range(45))
+    tempEn1 = en_abc[shift:] + en_abc[:shift]
+    tempEn2 = en_ABC[shift:] + en_ABC[:shift]
+    tempRu1 = ru_abc[shift:] + ru_abc[:shift]
+    tempRu2 = ru_ABC[shift:] + ru_ABC[:shift]
+    tempAlpha = tempEn1 + tempEn2 + tempRu1 + tempRu2
+    #print(tempAlpha)
+    #print(alphabet)
+    trans = str.maketrans(alphabet, tempAlpha)
+
+    ciphertext = plaintext.translate(trans)
+
     return ciphertext
 
-    #word = 'apple'
-    #print(encrypt_caesar(word))
+#word = 'PYTHON'
+#print(encrypt_caesar(word))
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
@@ -46,7 +55,15 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     >>> decrypt_caesar("")
     ''
     """
-    plaintext = ""
+    tempEn1 = en_abc[shift:] + en_abc[:shift]
+    tempEn2 = en_ABC[shift:] + en_ABC[:shift]
+    tempRu1 = ru_abc[shift:] + ru_abc[:shift]
+    tempRu2 = ru_ABC[shift:] + ru_ABC[:shift]
+    tempAlpha = tempEn1 + tempEn2 + tempRu1 + tempRu2
+
+    trans = str.maketrans(tempAlpha, alphabet)
+
+    plaintext = ciphertext.translate(trans)
     # PUT YOUR CODE HERE
     return plaintext
 
